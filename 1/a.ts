88eed18ce6f7d484094056adbab03cb4a2a71e7b
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-01 07:30:06 +0100

    day1

---
+++ 1/a.ts
@@ -0,0 +1,13 @@
+const input = await Deno.readTextFile("input.txt");
+
+const elves = input.split("\n\n").map((e) => e.split("\n"));
+
+let max = 0;
+elves.forEach((e) => {
+  const sum = e.reduce((a, b) => a + Number(b), 0);
+  if (sum > max) {
+    max = sum;
+  }
+});
+
+console.log("answer", max);
---
+++ 1/b.ts
@@ -0,0 +1,14 @@
+const input = await Deno.readTextFile("input.txt");
+
+const elves = input.split("\n\n").map((e) => e.split("\n"));
+
+let topThree = [0, 0, 0];
+elves.forEach((e) => {
+  const sum = e.reduce((a, b) => a + Number(b), 0);
+  if (sum > topThree[2]) {
+    topThree[2] = sum;
+    topThree.sort((a, b) => b - a);
+  }
+});
+
+console.log("answer", topThree[2] + topThree[1] + topThree[0]);
