7cc09ea183c6e12b1de0eb44fa96c8a4c125ccbf
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-02 08:42:29 +0100

    2

---
+++ 2/a.ts
@@ -0,0 +1,31 @@
+const input = await Deno.readTextFile("input.txt");
+
+const games = input.split("\n").map((e) => e.split(" "));
+
+console.log(games);
+let score = 0;
+
+games.forEach((game) => {
+  const [elf, me] = game;
+  if (elf === "A" && me === "X") {
+    score += 1 + 3;
+  } else if (elf === "A" && me === "Y") {
+    score += 2 + 6;
+  } else if (elf === "A" && me === "Z") {
+    score += 3 + 0;
+  } else if (elf === "B" && me === "Y") {
+    score += 2 + 3;
+  } else if (elf === "B" && me === "Z") {
+    score += 3 + 6;
+  } else if (elf === "B" && me === "X") {
+    score += 1 + 0;
+  } else if (elf === "C" && me === "Z") {
+    score += 3 + 3;
+  } else if (elf === "C" && me === "X") {
+    score += 1 + 6;
+  } else if (elf === "C" && me === "Y") {
+    score += 2 + 0;
+  }
+});
+
+console.log("answer", score);
---
+++ 2/b.ts
@@ -0,0 +1,32 @@
+const input = await Deno.readTextFile("input.txt");
+
+const games = input.split("\n").map((e) => e.split(" "));
+
+console.log(games);
+let score = 0;
+
+games.forEach((game) => {
+  const [elf, result] = game;
+
+  if (elf === "A" && result === "X") {
+    score += 3 + 0;
+  } else if (elf === "A" && result === "Y") {
+    score += 1 + 3;
+  } else if (elf === "A" && result === "Z") {
+    score += 2 + 6;
+  } else if (elf === "B" && result === "Y") {
+    score += 2 + 3;
+  } else if (elf === "B" && result === "Z") {
+    score += 3 + 6;
+  } else if (elf === "B" && result === "X") {
+    score += 1 + 0;
+  } else if (elf === "C" && result === "Z") {
+    score += 1 + 6;
+  } else if (elf === "C" && result === "X") {
+    score += 2 + 0;
+  } else if (elf === "C" && result === "Y") {
+    score += 3 + 3;
+  }
+});
+
+console.log("answer", score);
