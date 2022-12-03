b820c8dde8d22f11ce7bd553362d746f992f8ec3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-03 10:14:33 +0100

    3

---
+++ 3/a.ts
@@ -0,0 +1,21 @@
+const input = await Deno.readTextFile("input.txt");
+
+const rucksacks = input.split("\n");
+
+let score = 0;
+
+rucksacks.forEach((r) => {
+  const [one, two] = [r.slice(0, r.length / 2), r.slice(r.length / 2)];
+  const inBoth = one
+    .split("")
+    .filter((c) => two.includes(c))
+    .pop();
+
+  // a-z 1-26, A-Z 27-52
+  if (inBoth) {
+    const charCode = inBoth.charCodeAt(0);
+    score += charCode > 96 ? charCode - 96 : charCode - 38;
+  }
+});
+
+console.log(score);
---
+++ 3/b.ts
@@ -0,0 +1,24 @@
+const input = await Deno.readTextFile("input.txt");
+
+const rucksacks = input.split("\n");
+
+let score = 0;
+
+for (let i = 0; i < rucksacks.length; i += 3) {
+  const one = rucksacks[i];
+  const two = rucksacks[i + 1];
+  const three = rucksacks[i + 2];
+
+  const inAll = one
+    .split("")
+    .filter((c) => two.includes(c) && three.includes(c))
+    .pop();
+
+  // a-z 1-26, A-Z 27-52
+  if (inAll) {
+    const charCode = inAll.charCodeAt(0);
+    score += charCode > 96 ? charCode - 96 : charCode - 38;
+  }
+}
+
+console.log(score);
