26c98819e6415020e3d8939ef7a0c02ac60a6ad0
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-03 13:42:49 +0100

    hate that yellow thing

--- ts/1/b.ts
+++ ts/1/b.ts
@@ -2,7 +2,7 @@ const input = await Deno.readTextFile("../../input/1.txt");
 
 const elves = input.split("\n\n").map((e) => e.split("\n"));
 
-let topThree = [0, 0, 0];
+const topThree = [0, 0, 0];
 elves.forEach((e) => {
   const sum = e.reduce((a, b) => a + Number(b), 0);
   if (sum > topThree[2]) {
