be0a9f74508f2db7045e2217489f96b85f124a0e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-05 12:51:47 +0100

    5

---
+++ ts/5/b.ts
@@ -0,0 +1,36 @@
+const input = await Deno.readTextFile("../../input/5.txt");
+
+const [crates, moves] = input.split("\n\n");
+
+const horizontal = crates.split("\n").map((l) =>
+  // element 1, 5, 9, ...
+  l.split("").filter((_, i) => i % 4 === 1)
+);
+horizontal.pop(); // pop numbers at the end
+
+const stacks: string[][] = [];
+horizontal.reverse().forEach((h) => {
+  h.forEach((c, i) => {
+    if (!stacks[i]) {
+      stacks[i] = [];
+    }
+    if (c !== " ") stacks[i].push(c);
+  });
+});
+
+const movesList = moves
+  .split("\n")
+  .map((l) => l.split(" "))
+  .map((l) => ({
+    amount: Number(l[1]),
+    from: Number(l[3]) - 1, // 0 indexed
+    to: Number(l[5]) - 1, // 0 indexed
+  }));
+
+movesList.forEach((m) => {
+  const crates = stacks[m.from].splice(-m.amount);
+  stacks[m.to].push(...crates);
+});
+
+const word = stacks.map((s) => s.pop()).join("");
+console.log(word);
