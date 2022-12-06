294a9dbeadf8572e8b47777269cc589dc589f0b3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-06 12:46:03 +0100

    setup remaining

---
+++ ts/8/a.ts
@@ -0,0 +1,11 @@
+const input = await Deno.readTextFile("../../input/6.txt");
+
+const lines = input.split("\n");
+
+let answer = 0;
+
+lines.forEach((l) => {
+  // no-op
+});
+
+console.log("answer", answer);
