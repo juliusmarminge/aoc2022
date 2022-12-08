188ac35b27f9ba0acc60f35e03ec9e4362526123
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-08 11:40:21 +0100

    8

--- ts/08/a.ts
+++ ts/08/a.ts
@@ -1,11 +1,60 @@
 const input = await Deno.readTextFile("../../input/08.txt");
+// const input = await Deno.readTextFile("test.txt");
 
 const lines = input.split("\n");
 
-let answer = 0;
+const grid = lines.map((line) => line.split("").map(Number));
 
-lines.forEach((l) => {
-  // no-op
-});
+const visible = new Set();
 
-console.log("answer", answer);
+const visibleOnRow = (rowIdx: number) => {
+  const row = grid[rowIdx];
+  let tallest = -1;
+
+  for (let i = 0; i < row.length; i++) {
+    const cell = row[i];
+    if (cell > tallest) {
+      visible.add(`${rowIdx},${i}`);
+    }
+    tallest = Math.max(tallest, cell);
+  }
+
+  // same from right to left
+  tallest = -1;
+  for (let i = row.length - 1; i >= 0; i--) {
+    const cell = row[i];
+    if (cell > tallest) {
+      visible.add(`${rowIdx},${i}`);
+    }
+    tallest = Math.max(tallest, cell);
+  }
+};
+
+const visibleOnCol = (colIdx: number) => {
+  let tallest = -1;
+
+  for (let i = 0; i < grid.length; i++) {
+    const cell = grid[i][colIdx];
+    if (cell > tallest) {
+      visible.add(`${i},${colIdx}`);
+    }
+    tallest = Math.max(tallest, cell);
+  }
+
+  // same from bottom to top
+  tallest = -1;
+  for (let i = grid.length - 1; i >= 0; i--) {
+    const cell = grid[i][colIdx];
+    if (cell > tallest) {
+      visible.add(`${i},${colIdx}`);
+    }
+    tallest = Math.max(tallest, cell);
+  }
+};
+
+for (let i = 0; i < grid.length; i++) {
+  visibleOnRow(i);
+  visibleOnCol(i);
+}
+
+console.log("answer", visible.size);
--- ts/08/b.ts
+++ ts/08/b.ts
@@ -1,11 +1,40 @@
 const input = await Deno.readTextFile("../../input/08.txt");
-
 const lines = input.split("\n");
+const grid = lines.map((line) => line.split("").map(Number));
 
-let answer = 0;
+const getScenicScore = (rowIdx: number, colIdx: number) => {
+  const cellValue = grid[rowIdx][colIdx];
 
-lines.forEach((l) => {
-  // no-op
-});
+  let topScore = 0;
+  for (let i = rowIdx - 1; i >= 0; i--) {
+    topScore++;
+    if (grid[i][colIdx] >= cellValue) break;
+  }
 
-console.log("answer", answer);
+  let bottomScore = 0;
+  for (let i = rowIdx + 1; i < grid.length; i++) {
+    bottomScore++;
+    if (grid[i][colIdx] >= cellValue) break;
+  }
+
+  let leftScore = 0;
+  for (let i = colIdx - 1; i >= 0; i--) {
+    leftScore++;
+    if (grid[rowIdx][i] >= cellValue) break;
+  }
+
+  let rightScore = 0;
+  for (let i = colIdx + 1; i < grid[rowIdx].length; i++) {
+    rightScore++;
+    if (grid[rowIdx][i] >= cellValue) break;
+  }
+
+  return topScore * bottomScore * leftScore * rightScore;
+};
+
+const scenicScores = grid.map((row, rowIdx) =>
+  row.map((_, colIdx) => getScenicScore(rowIdx, colIdx))
+);
+
+const max = Math.max(...scenicScores.flat());
+console.log(max);
