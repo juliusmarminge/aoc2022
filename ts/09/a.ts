a30be4cd9652d1cbdd96a450411144c4822a6c8f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-09 09:23:04 +0100

    9

--- ts/09/a.ts
+++ ts/09/a.ts
@@ -1,11 +1,52 @@
 const input = await Deno.readTextFile("../../input/09.txt");
+// const input = await Deno.readTextFile("test.txt");
 
 const lines = input.split("\n");
 
-let answer = 0;
+const head = { x: 0, y: 0 };
+const tail = { x: 0, y: 0 };
+
+const tailVis = new Set([`${tail.x},${tail.y}`]);
 
 lines.forEach((l) => {
-  // no-op
+  const [dir, _steps] = l.split(" ");
+  const steps = parseInt(_steps);
+
+  for (let i = 0; i < steps; i++) {
+    switch (dir) {
+      case "U":
+        head.y++;
+        break;
+      case "D":
+        head.y--;
+        break;
+      case "L":
+        head.x--;
+        break;
+      case "R":
+        head.x++;
+        break;
+    }
+
+    const dx = head.x - tail.x;
+    const dy = head.y - tail.y;
+
+    if (dx > 1) {
+      tail.x++;
+      if (dy) tail.y += dy > 0 ? 1 : -1;
+    } else if (dx < -1) {
+      tail.x--;
+      if (dy) tail.y += dy > 0 ? 1 : -1;
+    } else if (dy > 1) {
+      tail.y++;
+      if (dx) tail.x += dx > 0 ? 1 : -1;
+    } else if (dy < -1) {
+      tail.y--;
+      if (dx) tail.x += dx > 0 ? 1 : -1;
+    }
+
+    tailVis.add(`${tail.x},${tail.y}`);
+  }
 });
 
-console.log("answer", answer);
+console.log("answer", tailVis.size);
--- ts/09/b.ts
+++ ts/09/b.ts
@@ -1,11 +1,67 @@
 const input = await Deno.readTextFile("../../input/09.txt");
+// const input = await Deno.readTextFile("test.txt");
 
 const lines = input.split("\n");
 
-let answer = 0;
+const knots = [
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+];
+
+const tailVis = new Set([`${knots[9].x},${knots[9].y}`]);
 
 lines.forEach((l) => {
-  // no-op
+  const [dir, _steps] = l.split(" ");
+  const steps = parseInt(_steps);
+
+  const head = knots[0];
+
+  for (let i = 0; i < steps; i++) {
+    switch (dir) {
+      case "U":
+        head.y++;
+        break;
+      case "D":
+        head.y--;
+        break;
+      case "L":
+        head.x--;
+        break;
+      case "R":
+        head.x++;
+        break;
+    }
+
+    for (let i = 0; i < knots.length - 1; i++) {
+      const dx = knots[i].x - knots[i + 1].x;
+      const dy = knots[i].y - knots[i + 1].y;
+
+      if (dx > 1) {
+        knots[i + 1].x++;
+        if (dy) knots[i + 1].y += dy > 0 ? 1 : -1;
+      } else if (dx < -1) {
+        knots[i + 1].x--;
+        if (dy) knots[i + 1].y += dy > 0 ? 1 : -1;
+      } else if (dy > 1) {
+        knots[i + 1].y++;
+        if (dx) knots[i + 1].x += dx > 0 ? 1 : -1;
+      } else if (dy < -1) {
+        knots[i + 1].y--;
+        if (dx) knots[i + 1].x += dx > 0 ? 1 : -1;
+      }
+
+      const tail = knots[knots.length - 1];
+      tailVis.add(`${tail.x},${tail.y}`);
+    }
+  }
 });
 
-console.log("answer", answer);
+console.log("answer", tailVis.size);
