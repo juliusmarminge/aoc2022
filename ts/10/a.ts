634ea310e3984afcaa8b64ee9ecd4808573aed0d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-10 13:04:55 +0100

    10

--- ts/10/a.ts
+++ ts/10/a.ts
@@ -1,11 +1,20 @@
 const input = await Deno.readTextFile("../../input/10.txt");
-
 const lines = input.split("\n");
 
-let answer = 0;
+const trace: number[] = [];
+for (let i = 0; i < lines.length; i++) {
+  const [op, val] = lines[i].split(" ");
+  trace.push(0);
+  if (op === "addx") trace.push(parseInt(val));
+}
 
-lines.forEach((l) => {
-  // no-op
+let x = 1;
+const prefixSum: number[] = [];
+trace.forEach((amount, i) => {
+  const instrCount = i + 1; // zero-based index
+  if ((instrCount + 20) % 40 === 0) prefixSum.push(x * instrCount);
+  x += amount;
 });
 
-console.log("answer", answer);
+const sum = prefixSum.reduce((a, b) => a + b, 0);
+console.log(sum);
--- ts/10/b.ts
+++ ts/10/b.ts
@@ -2,10 +2,29 @@ const input = await Deno.readTextFile("../../input/10.txt");
 
 const lines = input.split("\n");
 
-let answer = 0;
+const trace: number[] = [];
+for (let i = 0; i < lines.length; i++) {
+  const [op, val] = lines[i].split(" ");
+  trace.push(0);
+  if (op === "addx") trace.push(parseInt(val));
+}
 
-lines.forEach((l) => {
-  // no-op
+// create 6x40 array, filled with " "
+const [length, height] = [40, 6];
+const grid = Array.from({ length: height }, () =>
+  Array.from({ length }, () => " ")
+);
+
+// create the picture
+let spritePos = 1;
+trace.forEach((ins, index) => {
+  // get 2d pos by linear indexing
+  const x = index % length;
+  const y = Math.floor(index / length);
+
+  if (Math.abs(x - spritePos) <= 1) grid[y][x] = "#";
+
+  spritePos += ins;
+
+  if (x === length - 1) console.log(grid[y].join(""));
 });
-
-console.log("answer", answer);
