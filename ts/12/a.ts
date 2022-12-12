2dbf9814e222b9df3a8b3ad8dfe596348a800991
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-12 09:31:57 +0100

    12

--- ts/12/a.ts
+++ ts/12/a.ts
@@ -1,11 +1,50 @@
 const input = await Deno.readTextFile("../../input/12.txt");
 
-const lines = input.split("\n");
+let start = [0, 0] as [number, number],
+  end = [0, 0] as [number, number];
 
-let answer = 0;
+const grid = input.split("\n").map((line, row) =>
+  line.split("").map((char, col) => {
+    if (char === "S") {
+      start = [row, col];
+      return 0;
+    }
+    if (char === "E") {
+      end = [row, col];
+      return "z".charCodeAt(0) - 97;
+    }
+    return char.charCodeAt(0) - 97;
+  })
+);
 
-lines.forEach((l) => {
-  // no-op
-});
+const visited = new Set<string>();
 
-console.log("answer", answer);
+const queue = [start];
+let steps = 0;
+
+while (queue.length) {
+  const next = [] as [number, number][];
+
+  for (const [row, col] of queue) {
+    const key = `${row},${col}`;
+    if (visited.has(key)) continue;
+    visited.add(key);
+
+    if (row === end[0] && col === end[1]) {
+      console.log("answer", steps);
+      Deno.exit();
+    }
+
+    const elev = grid[row][col];
+
+    if (grid[row - 1]?.[col] - elev < 2) next.push([row - 1, col]);
+    if (grid[row + 1]?.[col] - elev < 2) next.push([row + 1, col]);
+    if (grid[row][col - 1] - elev < 2) next.push([row, col - 1]);
+    if (grid[row][col + 1] - elev < 2) next.push([row, col + 1]);
+  }
+
+  steps++;
+  queue.splice(0, queue.length, ...next);
+}
+
+console.log("no path found");
--- ts/12/b.ts
+++ ts/12/b.ts
@@ -1,11 +1,56 @@
 const input = await Deno.readTextFile("../../input/12.txt");
 
-const lines = input.split("\n");
+const starts = [] as [number, number][];
+let end = [0, 0] as [number, number];
 
-let answer = 0;
+const grid = input.split("\n").map((line, row) =>
+  line.split("").map((char, col) => {
+    if (char === "a" || char === "S") {
+      starts.push([row, col]);
+      return 0;
+    }
+    if (char === "E") {
+      end = [row, col];
+      return "z".charCodeAt(0) - 97;
+    }
+    return char.charCodeAt(0) - 97;
+  })
+);
 
-lines.forEach((l) => {
-  // no-op
-});
+const scores = [];
 
-console.log("answer", answer);
+for (let i = 0; i < starts.length; i++) {
+  const start = starts[i];
+
+  const visited = new Set<string>();
+
+  const queue = [start];
+  let steps = 0;
+
+  while (queue.length) {
+    const next = [] as [number, number][];
+
+    for (const [row, col] of queue) {
+      const key = `${row},${col}`;
+      if (visited.has(key)) continue;
+      visited.add(key);
+
+      if (row === end[0] && col === end[1]) {
+        scores.push(steps);
+        continue;
+      }
+
+      const elev = grid[row][col];
+
+      if (grid[row - 1]?.[col] - elev < 2) next.push([row - 1, col]);
+      if (grid[row + 1]?.[col] - elev < 2) next.push([row + 1, col]);
+      if (grid[row][col - 1] - elev < 2) next.push([row, col - 1]);
+      if (grid[row][col + 1] - elev < 2) next.push([row, col + 1]);
+    }
+
+    steps++;
+    queue.splice(0, queue.length, ...next);
+  }
+}
+
+console.log("scores", Math.min(...scores));
